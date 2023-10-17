d6056551aa8303f9ae22462e6a9a723980262017
Jera <73955437+jerapepe@users.noreply.github.com>
2023-10-17 06:44:01 +0000

    Avance

--- day1/day1.go
+++ day1/day1.go
@@ -4,33 +4,44 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"strings"
+	"strconv"
 )
 
 func main() {
 	file, err := os.Open("input.txt")
 	if err != nil {
-		fmt.Println("Error open:", err)
+		fmt.Println("Error:", err)
 		return
 	}
 	defer file.Close()
 	scanner := bufio.NewScanner(file)
-
-	nums := make([]string, 0)
-
+	
+	var lin []string
 	for scanner.Scan() {
-		linea := scanner.Text()
-
-		if linea == "" {
-			n := strings.Replace(linea, "", ",", -1)
-			nums = append(nums, n)
-		} else {
-			nums = append(nums, linea)
-		}
+		lin = append(lin, scanner.Text())
 	}
-	fmt.Println(nums)
 
 	if err := scanner.Err(); err != nil {
-		fmt.Println("Error al leer el archivo:", err)
+		fmt.Println("Error:", err)
+		return
 	}
+
+	may := 0
+	men := 0
+	for _, line := range lin {
+		if line == "" {
+			if men > may {
+				may = men
+			}
+			men = 0
+		} else {
+			num, err := strconv.Atoi(line)
+			if err != nil {
+				fmt.Println("Error \n", line, err)
+				return
+			}
+			men += num
+		}
+	}
+	fmt.Println(may)
 }
